f11b660ea352af6574bfbb1e622416f128bfda54
Zain <zainfaruqi9@gmail.com>
2025-02-27 21:37:32 -0500

    Added html, css, and js file

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.getElementById("container")
+
+
+for (let i = 0; i <= 20; i++) {
+    buttons = document.createElement("button")
+    buttons.classList.add("buttons")
+    container.appendChild(buttons)
+}
+
+console.log("Hello, World")
